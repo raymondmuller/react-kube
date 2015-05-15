c6259967e62f8f036195dee0dec1ace98954b7ff
rmuller17 <Xpl0sive>
2015-05-15 23:25:18 +0200

    updated readme

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,6 +1,7 @@
 const React = require("react");
 const FormSection = require("./lib/FormSection");
 const Grid = require("./lib/Grid");
+const Highlight = require("react-highlight");
 
 class GetStarted extends React.Component {
 
@@ -9,9 +10,16 @@ class GetStarted extends React.Component {
 			<div id="tab1">
 				<FormSection name="Get Started">
 					<Grid>
-						Work in progress! *Coming Soon*
-							<br/> <br/>
-						TODO: <br/>
+						Under active development | *Stay tuned!*
+						<hr/>
+							- npm install react-kube or bower install react-kube <br/>
+							- require the desired component to use it<br/><br/>
+							for example:
+							<Highlight>
+								&emsp;&emsp;{"const Alert = require(\"react-kube/Alert\")"}
+							</Highlight>
+						<hr/>
+						Todos: <br/>
 						- Add missing components (see <a href="https://github.com/raymondmuller/react-kube/issues" target="_new">Github Issues</a>) <br/>
 						- Remove jQuery and Kube.js dependencies <br/>
 						- Add Tests <br/>
